9ac5e133ee5dabaefa02c3d9ba8c2e56e910c747
einbecherkaffee <einbecherkaffee@gmail.com>
2017-12-04 20:46:30 +0100

    Pole renamed to Field

---
+++ Field.h
@@ -0,0 +1,47 @@
+#ifndef POLE_H
+#define POLE_H
+#include "conio2.h"
+#include <stdio.h>
+#include "Point.h"
+class Field {
+public:
+	char symbol;
+	Point p;
+	bool preset;
+	Field(int x, int y) : p(x, y) {
+		symbol = '.';
+		preset = false;
+	}
+	Field(int x, int y, char zn) : Field(x, y) {
+		symbol = zn;
+		preset = zn == '.' ? false : true;
+	}
+	void write() {
+		gotoxy(p.x + 2, p.y + 2);
+		putchar(symbol);
+	}
+	void write(int x, int y) {
+		gotoxy(p.x + x, p.y + y);
+		putchar(symbol);
+	}
+
+	void setChar(char c) {
+		if (preset == false) {
+			symbol = c;
+		}
+	}
+	void setPresetChar(char c) {
+		if (preset == false) {
+			symbol = c;
+			preset = true;
+		}
+	}
+	int x() {
+		return p.x;
+	}
+	int y() {
+		return p.y;
+	}
+
+};
+#endif // !POLE_H
